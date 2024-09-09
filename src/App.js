e7a708899aeecf3a6f4f3c126114325740fcebef
ayu-sh-raj <114801342+ayu-sh-raj@users.noreply.github.com>
2024-09-09 07:49:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,450 @@
+import './App.css';
+
+function App() {
+  return (
+<div>
+<nav>
+<img src="https://miro.medium.com/max/900/0*9hcinRdaHicrNpNE.jpg" class="logo"/>
+<div class="linklist">
+<a class="link"> Home </a>
+<a class="link"> Shop </a>
+<a class="link"> Customize </a>
+<a class="link"><i class="fa-solid fa-user"></i> </a>
+<a class="menu link cartnav"><i class="fa-solid fa-cart-shopping"></i></a>
+<button class="menu link"><i class="fa-solid fa-bars"></i></button>
+</div>
+</nav>
+
+<div class="sidebar">
+  <a href="" class="menuitem"><i class="fa-solid fa-house"></i> Home</a>
+  <a href="" class="menuitem"><i class="fa-solid fa-bag-shopping"></i> Shop</a>
+  <a href="" class="menuitem"> Customize</a>
+  <a href="" class="menuitem"><i class="fa-solid fa-user"></i>  Login</a>
+  <center><button class="menuitem closebtn"><i class="fa-solid fa-xmark"></i> Close</button></center>
+</div>
+
+<center>
+<div class="banner">
+  <div class="art"> </div>
+  <img src="https://static.nike.com/a/images/t_default/7c578cde-85b3-4d9c-bc1d-989f03418f3e/lebron-19-basketball-shoes-VkqHgW.png" class="bannershoe"/>
+  <span class="bannertxt">Just Do It. </span>
+   <div class="space"></div>
+</div>
+</center>
+
+
+<h1 class="sectitle">Sneakers</h1>
+<div class="row">
+  <button class="move-left">{'<'}</button>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/3c20bb9e-48d2-44e4-bb2a-abe502b11b50/air-max-270-shoes-s1JpCx.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max 270</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+
+<br/>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/0461fc94-4cb8-4d34-a50a-e9c0a18ef578/zoom-fly-5-road-running-shoes-lkx7Zp.png"class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Zoom fly</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+  
+ <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/b312cb23-4862-4aa8-bd30-66f94ec26c71/air-max-dawn-shoe-VbDN84.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max Dawn</b><br/>
+    <span class="price">$120.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+
+<br/>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/0461fc94-4cb8-4d34-a50a-e9c0a18ef578/zoom-fly-5-road-running-shoes-lkx7Zp.png"class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Zoom fly</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+ 
+  <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/b312cb23-4862-4aa8-bd30-66f94ec26c71/air-max-dawn-shoe-VbDN84.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max Dawn</b><br/>
+    <span class="price">$120.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+ 
+ <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/3c20bb9e-48d2-44e4-bb2a-abe502b11b50/air-max-270-shoes-s1JpCx.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max 270</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+
+<br/>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/0461fc94-4cb8-4d34-a50a-e9c0a18ef578/zoom-fly-5-road-running-shoes-lkx7Zp.png"class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Zoom fly</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+  
+ <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/b312cb23-4862-4aa8-bd30-66f94ec26c71/air-max-dawn-shoe-VbDN84.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max Dawn</b><br/>
+    <span class="price">$120.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+
+<br/>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/0461fc94-4cb8-4d34-a50a-e9c0a18ef578/zoom-fly-5-road-running-shoes-lkx7Zp.png"class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Zoom fly</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+ 
+  <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/b312cb23-4862-4aa8-bd30-66f94ec26c71/air-max-dawn-shoe-VbDN84.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max Dawn</b><br/>
+    <span class="price">$120.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+    
+    <button class="move-right">{'>'}</button>
+  
+</div>
+
+
+<h1 class="sectitle">Men's Sneakers</h1>
+<div class="row">
+  <button class="move-left">{'<'}</button>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/3c20bb9e-48d2-44e4-bb2a-abe502b11b50/air-max-270-shoes-s1JpCx.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max 270</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+
+<br/>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/0461fc94-4cb8-4d34-a50a-e9c0a18ef578/zoom-fly-5-road-running-shoes-lkx7Zp.png"class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Zoom fly</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+  
+ <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/b312cb23-4862-4aa8-bd30-66f94ec26c71/air-max-dawn-shoe-VbDN84.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max Dawn</b><br/>
+    <span class="price">$120.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+
+<br/>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/0461fc94-4cb8-4d34-a50a-e9c0a18ef578/zoom-fly-5-road-running-shoes-lkx7Zp.png"class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Zoom fly</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+ 
+  <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/b312cb23-4862-4aa8-bd30-66f94ec26c71/air-max-dawn-shoe-VbDN84.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max Dawn</b><br/>
+    <span class="price">$120.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+ 
+ <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/3c20bb9e-48d2-44e4-bb2a-abe502b11b50/air-max-270-shoes-s1JpCx.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max 270</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+
+<br/>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/0461fc94-4cb8-4d34-a50a-e9c0a18ef578/zoom-fly-5-road-running-shoes-lkx7Zp.png"class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Zoom fly</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+  
+ <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/b312cb23-4862-4aa8-bd30-66f94ec26c71/air-max-dawn-shoe-VbDN84.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max Dawn</b><br/>
+    <span class="price">$120.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+
+<br/>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/0461fc94-4cb8-4d34-a50a-e9c0a18ef578/zoom-fly-5-road-running-shoes-lkx7Zp.png"class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Zoom fly</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+ 
+  <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/b312cb23-4862-4aa8-bd30-66f94ec26c71/air-max-dawn-shoe-VbDN84.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max Dawn</b><br/>
+    <span class="price">$120.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+    
+    <button class="move-right">{'>'}</button>
+  
+</div>
+
+
+<h1 class="sectitle">Women's Sneakers</h1>
+<div class="row">
+  <button class="move-left">{'<'}</button>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/3c20bb9e-48d2-44e4-bb2a-abe502b11b50/air-max-270-shoes-s1JpCx.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max 270</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+
+<br/>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/0461fc94-4cb8-4d34-a50a-e9c0a18ef578/zoom-fly-5-road-running-shoes-lkx7Zp.png"class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Zoom fly</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+  
+ <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/b312cb23-4862-4aa8-bd30-66f94ec26c71/air-max-dawn-shoe-VbDN84.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max Dawn</b><br/>
+    <span class="price">$120.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+
+<br/>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/0461fc94-4cb8-4d34-a50a-e9c0a18ef578/zoom-fly-5-road-running-shoes-lkx7Zp.png"class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Zoom fly</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+ 
+  <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/b312cb23-4862-4aa8-bd30-66f94ec26c71/air-max-dawn-shoe-VbDN84.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max Dawn</b><br/>
+    <span class="price">$120.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+ 
+ <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/3c20bb9e-48d2-44e4-bb2a-abe502b11b50/air-max-270-shoes-s1JpCx.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max 270</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+
+<br/>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/0461fc94-4cb8-4d34-a50a-e9c0a18ef578/zoom-fly-5-road-running-shoes-lkx7Zp.png"class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Zoom fly</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+  
+ <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/b312cb23-4862-4aa8-bd30-66f94ec26c71/air-max-dawn-shoe-VbDN84.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max Dawn</b><br/>
+    <span class="price">$120.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+
+<br/>
+
+<div class="card">
+  <img src="https://static.nike.com/a/images/t_default/0461fc94-4cb8-4d34-a50a-e9c0a18ef578/zoom-fly-5-road-running-shoes-lkx7Zp.png"class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Zoom fly</b><br/>
+    <span class="price">$20.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+ 
+  <div class="card">
+  <img src="https://static.nike.com/a/images/t_default/b312cb23-4862-4aa8-bd30-66f94ec26c71/air-max-dawn-shoe-VbDN84.png" class="shoe"/>
+  <center>
+  <p class="desc">
+    <b>Nike Air Max Dawn</b><br/>
+    <span class="price">$120.20</span> 
+    <span class="rate"> &#11088;(4.5)</span><br/>
+    <button class="cart">Add to Cart</button>
+  </p>
+  </center>
+</div>
+    
+    <button class="move-right">{'>'}</button>
+  
+</div>
+
+</div>
+  );
+}
+
+export default App;
